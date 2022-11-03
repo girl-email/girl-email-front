bd56400fc3f920116ff910d2e8658bdcaede4a1d
马文卿 <529837505@qq.com>
2022-11-03 19:14:41 +0800

    feat: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    settings: {
+        react: {
+            version: "999.999.999",
+        },
+    },
+    overrides: [],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react", "@typescript-eslint"],
+    rules: {
+        quotes: ["error", "single"],
+        semi: ["error", "always"],
+        "react/react-in-jsx-scope": "off",
+    },
+};
