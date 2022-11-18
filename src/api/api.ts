c4ca3a27b84c2c4c7bc6e27c96e34522a170d916
马文卿 <529837505@qq.com>
2022-11-18 13:30:07 +0800

    chore: send task config interface

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,15 +1,19 @@
 import http from './http';
-import { LoginReq, LoginRes, RegisterReq, RegisterRes, SendRegisterEmailReq } from '@/types/api';
+import * as h from '@/types/api';
 
 /** ***************************** login ****************************/
-export const USER_LOGIN = async (params: LoginReq) => {
-    return await http<LoginRes>('/api/admin/base/open/login', 'post', params);
+export const USER_LOGIN = async (params: h.LoginReq) => {
+    return await http<h.LoginRes>('/api/admin/base/open/login', 'post', params);
 };
 
-export const USER_REGISTER = async (params: RegisterReq) => {
-    return await http<RegisterRes>('/api/admin/base/open/register', 'post', params);
+export const USER_REGISTER = async (params: h.RegisterReq) => {
+    return await http<h.RegisterRes>('/api/admin/base/open/register', 'post', params);
 };
 
-export const SEND_REGISTER_EMAIL = async (params: SendRegisterEmailReq) => {
+export const SEND_REGISTER_EMAIL = async (params: h.SendRegisterEmailReq) => {
     return await http('/api/admin/base/open/sendEmail', 'post', params);
+};
+
+export const SEND_TASK_CONFIG = async (params: h.sendTaskConfigReq) => {
+    return await http<h.sendTaskConfigRes>('/api/admin/email/conf/add', 'post', params);
 };
--- src/types/api.ts
+++ src/types/api.ts
@@ -1,40 +1,54 @@
 export interface LoginReq {
-    email: string,
+    email: string;
     password: string
 }
 
 export interface LoginRes {
-    expire: number,
-    email: string,
-    token: string,
-    refreshExpire: number,
+    expire: number;
+    email: string;
+    token: string;
+    refreshExpire: number;
     refreshToken: string
 }
 
 export interface RegisterReq {
-    code: string,
-    email: string,
+    code: string;
+    email: string;
     password: string
 }
 
 export interface RegisterRes {
-    password: string,
-    email: string,
-    username: string,
-    departmentId: null,
-    name: null,
-    nickName: null,
-    headImg: null,
-    phone: null,
-    remark: null,
-    socketId: null,
-    id: number,
-    createTime: string,
-    updateTime: string,
-    passwordV: number,
+    password: string;
+    email: string;
+    username: string;
+    departmentId: null;
+    name: null;
+    nickName: null;
+    headImg: null;
+    phone: null;
+    remark: null;
+    socketId: null;
+    id: number;
+    createTime: string;
+    updateTime: string;
+    passwordV: number;
     status: number
 }
 
 export interface SendRegisterEmailReq {
     email: string
+}
+
+export interface sendTaskConfigReq {
+    from_user: string;
+    from_email: string;
+    city: string;
+    to_user: string;
+    to_email: string;
+    startDay: string;
+    email_subject: string;
+}
+
+export interface sendTaskConfigRes {
+    id: number
 }
