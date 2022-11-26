250470080ee94e98dc87a6c3b2df27aac8c27c77
马文卿 <529837505@qq.com>
2022-11-26 22:29:23 +0800

    chore: add task card

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,9 +3,26 @@ import styles from './index.module.less';
 
 const Home = () => {
 
+    const renderTaskCard = () => {
+        return (
+            <Fragment>
+                <div className={styles.home_task_card}>
+                    <ul>
+                        <li>
+                            <span>标题：</span>
+                            <span></span>
+                        </li>
+                    </ul>
+                </div>
+            </Fragment>
+        );
+    };
+
     return (
         <Fragment>
-            <div className={styles.home_main}></div>
+            <div className={styles.home_main}>
+                {renderTaskCard()}
+            </div>
         </Fragment>
     );
 };
